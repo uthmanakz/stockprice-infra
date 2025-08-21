109a18808f2401b0d53c66b434d2461a436e938a
Uthman Akinwumi <uthmanakz@Uthmans-MacBook-Pro.local>
2025-08-21 20:32:51 +0100

    added output for kubectl ip

--- output.tf
+++ output.tf
@@ -18,3 +18,21 @@ output "eks_cluster_ca" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "kubectl_public_ip" {
+  value = aws_instance.kubectl_machine.public_ip
+}
+
+resource "aws_instance" "kubectl_machine" {
+  ami           = "ami-071899a54a905868f" # Amazon Linux 2 (us-west-2); update if using another region
+  instance_type = "t2.micro"
+
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.kubectl_sg.id]
+  associate_public_ip_address = true
+  key_name               = "uthmanakz" # Replace with your EC2 key pair name
+
+  tags = {
+    Name = "kubectl-bastion"
+  }
+}
